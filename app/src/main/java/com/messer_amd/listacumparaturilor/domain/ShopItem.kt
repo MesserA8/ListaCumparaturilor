ac1a539fd00560dffb6af31feccedb7b4973cbf5
MesserA8 <atihon09@gmail.com>
2022-05-19 14:10:14 +0300

    Create_comp-object-in_ShopItem-dataclass

--- app/src/main/java/com/messer_amd/listacumparaturilor/domain/ShopItem.kt
+++ app/src/main/java/com/messer_amd/listacumparaturilor/domain/ShopItem.kt
@@ -4,5 +4,11 @@ data class ShopItem(
     val name: String,
     val count: Float,
     val enabled: Boolean,
-    var id: Int = -1
+    var id: Int = UNDEFINED_ID
 )
+{
+    companion object{
+
+        const val UNDEFINED_ID = -1
+    }
+}
