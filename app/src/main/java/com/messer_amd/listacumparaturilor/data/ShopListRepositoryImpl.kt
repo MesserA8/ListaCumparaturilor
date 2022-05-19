77dfebc4302d03ba2b4da93af54bf7b9f6d9daa8
MesserA8 <atihon09@gmail.com>
2022-05-19 13:40:55 +0300

    Create_object-ShopListRepositoryImpl-package_data

---
+++ app/src/main/java/com/messer_amd/listacumparaturilor/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.messer_amd.listacumparaturilor.data
+
+import com.messer_amd.listacumparaturilor.domain.ShopItem
+import com.messer_amd.listacumparaturilor.domain.ShopListRepository
+
+object ShopListRepositoryImpl: ShopListRepository {
+    override fun addShopItem(shopItem: ShopItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        TODO("Not yet implemented")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        TODO("Not yet implemented")
+    }
+}
