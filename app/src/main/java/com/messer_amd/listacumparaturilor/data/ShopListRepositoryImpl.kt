305378edce9045f7a0bc6fd7bd40d5689d92ee5d
MesserA8 <atihon09@gmail.com>
2022-05-23 14:05:34 +0300

    Item-shop elements added to activity

--- app/src/main/java/com/messer_amd/listacumparaturilor/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/messer_amd/listacumparaturilor/data/ShopListRepositoryImpl.kt
@@ -1,9 +1,13 @@
 package com.messer_amd.listacumparaturilor.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.messer_amd.listacumparaturilor.domain.ShopItem
 import com.messer_amd.listacumparaturilor.domain.ShopListRepository
 
 object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopListLD = MutableLiveData<List<ShopItem>>()
 
     private val shopList = mutableListOf<ShopItem>()
 
@@ -21,10 +25,12 @@ object ShopListRepositoryImpl : ShopListRepository {
             shopItem.id = autoIncrementId++
         }
         shopList.add(shopItem)
+        updateList()
     }
 
     override fun deleteShopItem(shopItem: ShopItem) {
         shopList.remove(shopItem)
+        updateList()
     }
 
     override fun editShopItem(shopItem: ShopItem) {
@@ -38,7 +44,11 @@ object ShopListRepositoryImpl : ShopListRepository {
             ?: throw RuntimeException("Element with id $shopItemId not found")
     }
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toList()
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListLD
+    }
+
+    private fun updateList(){
+        shopListLD.value = shopList.toList()
     }
 }
--- app/src/main/java/com/messer_amd/listacumparaturilor/presentation/MainActivity.kt
+++ app/src/main/java/com/messer_amd/listacumparaturilor/presentation/MainActivity.kt
@@ -3,20 +3,37 @@ package com.messer_amd.listacumparaturilor.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
 import androidx.lifecycle.ViewModelProvider
 import com.messer_amd.listacumparaturilor.R
+import com.messer_amd.listacumparaturilor.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var llShopList: LinearLayout
     private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java] //equals: (this).get(MainViewModel::class.java)
-        viewModel.shopList.observe(this){
-            Log.d("MyTest", it.toString())
+        llShopList = findViewById(R.id.ll_shop_list)
+        viewModel =
+            ViewModelProvider(this)[MainViewModel::class.java] //equals: (this).get(MainViewModel::class.java)
+        viewModel.shopList.observe(this) {
+            showList(it)
         }
-        viewModel.getShopList()
+    }
+
+    private fun showList(list: List<ShopItem>) {
+        for (shopItem in list) {
+            val layoutId = if (shopItem.enabled) {
+                R.layout.item_shop_enabled
+            } else {
+                R.layout.item_shop_disabled
+            }
+            val view = LayoutInflater.from(this).inflate(layoutId, llShopList, false)
+            llShopList.addView(view)
+        }
     }
 }
