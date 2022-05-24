039a606939f4770cf036fe95b4a15ebe12a1cd19
MesserA8 <atihon09@gmail.com>
2022-05-24 12:31:43 +0300

    Create adapter

--- app/src/main/java/com/messer_amd/listacumparaturilor/presentation/MainActivity.kt
+++ app/src/main/java/com/messer_amd/listacumparaturilor/presentation/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import com.messer_amd.listacumparaturilor.R
 import com.messer_amd.listacumparaturilor.domain.ShopItem
@@ -26,6 +27,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showList(list: List<ShopItem>) {
+        llShopList.removeAllViews()
         for (shopItem in list) {
             val layoutId = if (shopItem.enabled) {
                 R.layout.item_shop_enabled
@@ -33,6 +35,14 @@ class MainActivity : AppCompatActivity() {
                 R.layout.item_shop_disabled
             }
             val view = LayoutInflater.from(this).inflate(layoutId, llShopList, false)
+            val tvName = view.findViewById<TextView>(R.id.tv_name)
+            val tvCount = view.findViewById<TextView>(R.id.tv_count)
+            tvName.text = shopItem.name
+            tvCount.text = shopItem.count.toString()
+            view.setOnLongClickListener {
+                viewModel.changeEnabledState(shopItem)
+                true
+            }
             llShopList.addView(view)
         }
     }
